c812cd1cb6770092c1e53bb4e850401575307625
Dmitrii Shkliaev <ds.shklyaev@gmail.com>
2024-10-04 05:08:14 +0300

    Первая загрузка проекта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+//Функция создания карточки
+function createCard(cardData, removeCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  const removeCardButton = cardElement.querySelector('.card__delete-button');
+  removeCardButton.addEventListener('click', removeCard);
+
+  return cardElement;
+}
+
+// Функция для удаления карточки
+function removeCard(event) {
+  const elementToRemove = event.target.closest('.places__item');
+  elementToRemove.remove();
+}
+
+// Выводим карточки на страницу
+function createCards() {
+  for (let i = 0; i < initialCards.length; i++) {
+    placesList.append(createCard(initialCards[i], removeCard))
+  }
+}
+
+createCards()
